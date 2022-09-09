5f64337c1d9ad3f050b4b95d9c292cd6893db208
Scoppatumana <boluwasodiq314@gmail.com>
2022-09-09 13:32:50 -0700

    Second confirmation check

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -9,25 +9,34 @@
 
 int main(void)
 {
-		int n;
+	int n;
 
-			srand(time(0));
-				n = rand() - RAND_MAX / 2;
+	srand(time(0));
 
-					printf("Last digit of %i is %i and is ", n, (n % 10));
+	n = rand() - RAND_MAX / 2;
 
-						if ((n % 10) == 0)
-								{
-											printf("0\n");
-												}
-							else if ((n % 10) > 5)
-									{
-												printf("greater than 5\n");
-													}
-								else
-										{
-													printf("less than 6 and not 0\n");
-														}
+	printf("Last digit of %i is %i and is ", n, (n % 10));
 
-									return (0);
+	if ((n % 10) == 0)
+	{
+		printf("0\n");
+	}
+		
+	else if ((n % 10) > 5)
+		
+	{
+	
+		printf("greater than 5\n");
+		
+	}
+
+	else
+
+	{
+
+		printf("less than 6 and not 0\n");
+
+	}
+
+	return (0);
 }
