3be174726b9e31ee46362c9e87631e7755059a8f
Scoppatumana <boluwasodiq314@gmail.com>
2022-09-09 16:56:14 -0700

    last commit

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -10,6 +10,7 @@ int main(void)
 	int one;
 	int ten;
 	int hundred;
+
 	for (hundred = '0'; hundred <= '9'; hundred++) /*hundreds place*/
 	{
 	for (ten = (hundred + 1); ten <= '9'; ten++) /*tens=100s+1*/
@@ -19,7 +20,7 @@ int main(void)
 	putchar(hundred);
 	putchar(ten);
 	putchar(one);
-																		if (hundred != '7' || ten != '8' || one != '9')
+	if (hundred != '7' || ten != '8' || one != '9')
 	{
 	putchar(',');
 	putchar(' ');
