ed536eb0588fb7131fc02c0e7edeeff92409e80f
Scoppatumana <boluwasodiq314@gmail.com>
2022-09-09 18:41:13 -0700

    Shishe jhoor

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -1,32 +1,34 @@
 #include <stdio.h>
 
 /**
- * main - prints all possible combinations of two two-digit numbers
- * Return: 0 (Success)
-*/
+ *  *main - print all combinations of three different digits
+ *  *Return: Always 0 (Success)
+ */
+
 int main(void)
 {
-	int i, j;
+		int one;
+			int ten;
+				int hundred;
 
-	for (i = 0; i < 100; i++)
-	{
-		for (j = 0; j < 100; j++)
-		{
-			if (i < j)
-			{
-				putchar((i / 10) + 48);
-				putchar((i % 10) + 48);
-				putchar(' ');
-				putchar((j / 10) + 48);
-				putchar((j % 10) + 48);
-				if (i != 98 || j != 99)
-				{
-					putchar(',');
-					putchar(' ');
-				}
-			}
-		}
-	}
-	putchar('\n');
-	return (0);
+					for (hundred = '0'; hundred <= '9'; hundred++) /*hundreds place*/
+							{
+									for (ten = (hundred + 1); ten <= '9'; ten++) /*tens=100s+1*/
+											{
+													for (one = (ten + 1); one <= '9'; one++) /*ones*/
+															{
+																	putchar(hundred);
+																		putchar(ten);
+																			putchar(one);
+																				if (hundred != '7' || ten != '8' || one != '9')
+																						{
+																								putchar(',');
+																									putchar(' ');
+																										}
+																					}
+														}
+										}
+						putchar('\n');
+
+							return (0);
 }
