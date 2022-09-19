33edf4db8c43f1cf09a7d1047fc989b831a5c252
Scoppatumana <boluwasodiq314@gmail.com>
2022-09-19 15:56:34 -0700

    last commit last last

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -1,53 +1,24 @@
 #include "main.h"
-
+#include <stdio.h>
 /**
- * _strlen - returns the length of a string
- * @s: string
- * Return: length
- */
-
-int _strlen(char *s)
-{
-	int len = 0;
-
-	while (*s != '\0')
-	{
-		len++;
-		s++;
-	}
-
-	return (len);
-}
-
-/**
- * _strcpy - copies the string pointed to by src,
- * including the terminating null byte (\0),
- * to the buffer pointed to by dest
- * @dest: copy source to this buffer
- * @src: this is the source to copy
- * Return: copy of original source
- */
-
-char *_strcpy(char *dest, char *src)
-{
-	int i;
-
-	for (i = 0; i <= _strlen(src); i++)
-		dest[i] = src[i];
-
-	return (dest);
-}
-
-/* another method using pointer notation
-
-char *_strcpy(char *dest, char *src)
-{
-        while (*src)
-	{
- 	   *dest = *src;
-	   src++;
-	   dest++;
-	}
-	return (dest);
-}
+ *  * _strcpy - copies the string pointed to by src,
+ *   * including the terminating null byte, to the
+ *    * buffer pointed to by dest.
+ *     * @dest: destination.
+ *      * @src: source.
+ *       * Return: the pointer to dest.
 */
+char *_strcpy(char *dest, char *src)
+{
+	int count = 0;
+
+	while (count >= 0)
+	{
+	*(dest + count) = *(src + count);
+	if (*(src + count) == '\0')
+	break;
+	count++;
+	}
+	
+	return (dest);
+}
